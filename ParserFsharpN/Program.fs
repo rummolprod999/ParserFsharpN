feccd94f3613a9256be7024afb0abd4cfeaf4c56
rummolprod999 <rummolprod999@gmail.com>
2019-04-03 15:25:59 +0300

    init

---
+++ ParserFsharpN/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+namespace ParserFsharp
+open System
+        
+module EntryPoint =
+        let arguments = "pcontr223 last, pcontr223 daily"    
+        [<EntryPoint>]
+        let main argv =
+            if argv.Length = 0 then
+                printf "Bad arguments, use %s" arguments
+                Environment.Exit(1)
+            0 
+
