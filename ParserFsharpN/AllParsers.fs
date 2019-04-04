bf10af2702675e749669cf738125372e258656c5
rummolprod999 <rummolprod999@gmail.com>
2019-04-04 15:31:37 +0300

    update

---
+++ ParserFsharpN/AllParsers.fs
@@ -0,0 +1,7 @@
+namespace ParserFsharp
+
+module P =
+    
+    let parserPContr (dir: string) =
+        ()
+
--- ParserFsharpN/Executor.fs
+++ ParserFsharpN/Executor.fs
@@ -3,12 +3,15 @@ open System
 
 module Executor =
     let arguments = "pcontr223 last, pcontr223 daily"
-    
+
     let parserArgs = function
-                     | [|"pcontr223"; "last"|] -> S.argTuple <- Argument.Pcotntr223("last")
-                     | [|"pcontr223"; "daily"|] -> S.argTuple <- Argument.Pcotntr223("daily")
+                     | [| "pcontr223"; "last" |] -> S.argTuple <- Argument.Pcotntr223("last")
+                     | [| "pcontr223"; "daily" |] -> S.argTuple <- Argument.Pcotntr223("daily")
                      | _ -> printf "Bad arguments, use %s" arguments
                             Environment.Exit(1)
     let parser = function
-                 | Pcotntr223 d -> ()
+                 | Pcotntr223 d ->
+                     try
+                         P.parserPContr d
+                     with ex -> Logging.Log.logger ex
                  | _ -> ()
